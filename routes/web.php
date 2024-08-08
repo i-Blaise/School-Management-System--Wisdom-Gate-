045335678c1147285f9f0951e3cca16719a4fb04
Your Name <menniablaise@yahoo.com>
2024-08-08 21:18:04 +0000

    push

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('home');
 
 Route::get('/login', function () {
     return view('auth.login');
 });
 Route::get('/add-student', function () {
     return view('pages.students.add-student');
-});
+})->name('add-student');
